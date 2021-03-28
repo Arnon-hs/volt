9ef0b3f66200c190cd43ac5921ce244b2f19ec8b
root <root@volt.monteops.com>
2021-03-28 19:06:53 +0000

    initial

---
+++ sync.sh
@@ -0,0 +1,3 @@
+rsync --progress -rvht --delete-after  root@volt.monteops.com:/var/www/vhosts/volt.monteops.com/* ./
+rsync --progress -rvht --delete-after  root@volt.monteops.com:/var/www/vhosts/volt.monteops.com/.[^.]* ./
+rsync --progress -rvht --delete-after  root@volt.monteops.com:/var/www/vhosts/volt.monteops.com/.git/* ./.git/ 
